547a3910802e9e6409d22411d98160932df5b2fc
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-08 14:30:42 -0400

    Added Note Selector and Edit Pages

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,24 @@
+<script>
+import NoteSelectors from "./NoteSelectors.vue";
+
+export default {
+  components: {
+    NoteSelectors,
+  },
+};
+</script>
+
+<template>
+  <div class="note-container">
+    <NoteSelectors />
+    <div class="note-editor">
+      <p class="note-editor-info">Timestamp here...</p>
+      <textarea class="note-editor-input">
+          First note...
+
+          Note text here...
+        </textarea
+      >
+    </div>
+  </div>
+</template>
---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: ["note"],
+};
+</script>
+
+<template>
+  <div class="note-selector active">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp"></p>
+  </div>
+  <template />
+</template>
